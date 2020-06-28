d7ab8eebb2e80bef0b7d87fa66bfd92387690f72
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-28 02:13:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word) #/[aeiou]/
 if word.match(//[aeiou]/)
-  true 
-else 
-  fasle 
-end 
+  true
+else
+  fasle
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
