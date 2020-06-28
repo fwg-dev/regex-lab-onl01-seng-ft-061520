b2959a2437d7641591e9b17e1eb35aef609faebe
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-28 02:08:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-
+word.match(/\starts_with_a_vowel/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -17,3 +17,28 @@ end
 def valid_phone_number?(phone)
 
 end
+
+
+# #starts_with_a_vowel?
+#   returns true for words starting with a vowel (FAILED - 1)
+#   returns false for words starting with a consonant (FAILED - 2)
+#   #words_starting_with_un_and_ending_with_ing
+#     returns an array with the words starting with 'un' and ending with 'ing' (FAILED - 3)
+#   #words_five_letters_long
+#     returns an array of words that are five letters long (FAILED - 4)
+# 
+#     #first_word_capitalized_and_ends_with_punctuation?
+#     Returns true for text starting with a capital letter and ending with puncutation (FAILE
+# D - 5)
+# 
+# Returns false for text starting with an uncapitalized letter but ending with puncutatio
+# n (FAILED - 6)
+# 
+#     Returns false for text starting with a capital letter but ending without puncutation (F
+#     AILED - 7)
+#         Returns false for text starting without a capital letter and ending without puncutation
+#      (FAILED - 8)
+#       #valid_phone_number?
+#         returns true for valid phone numbers, regardless of formatting (FAILED - 9)
+#         returns false for invalid phone numbers, regardless of formatting
+#     
