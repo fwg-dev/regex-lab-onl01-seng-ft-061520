e0689a3fcf5b193ad23342d78a5b527335f38976
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-28 02:21:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,5 @@
 def starts_with_a_vowel?(word) #/[aeiou]/
+  #.match returns first item in your string that matches a given Regular Expression as a match
 if word.match(/[aeiou]/)
   true
 else
@@ -7,6 +8,7 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  #.scan returns an array of all items in your string that match a given Regular Expressions 
   if text.scan(/un\w+ing/)
   true
 else
@@ -24,28 +26,3 @@ end
 def valid_phone_number?(phone)
 
 end
-
-
-# #starts_with_a_vowel?
-#   returns true for words starting with a vowel (FAILED - 1)
-#   returns false for words starting with a consonant (FAILED - 2)
-#   #words_starting_with_un_and_ending_with_ing
-#     returns an array with the words starting with 'un' and ending with 'ing' (FAILED - 3)
-#   #words_five_letters_long
-#     returns an array of words that are five letters long (FAILED - 4)
-#
-#     #first_word_capitalized_and_ends_with_punctuation?
-#     Returns true for text starting with a capital letter and ending with puncutation (FAILE
-# D - 5)
-#
-# Returns false for text starting with an uncapitalized letter but ending with puncutatio
-# n (FAILED - 6)
-#
-#     Returns false for text starting with a capital letter but ending without puncutation (F
-#     AILED - 7)
-#         Returns false for text starting without a capital letter and ending without puncutation
-#      (FAILED - 8)
-#       #valid_phone_number?
-#         returns true for valid phone numbers, regardless of formatting (FAILED - 9)
-#         returns false for invalid phone numbers, regardless of formatting
-#
